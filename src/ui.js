5d0e12d49b3f1d9607385ae2a96236dbbcb5176d
carlos505 <aragonaugusto24@gmail.com>
2023-09-01 08:57:45 -0600

    Change sidebar behavior

--- src/ui.js
+++ src/ui.js
@@ -4,8 +4,9 @@ export function renderUi () {
     const mainContent = document.getElementById('main-content')
     const footerContent = document.getElementById('footer-content')
     const currentEl = document.getElementById('current-el')
-    const currentElChild = document.getElementById('current-el-child')
+    const toHourEl = document.getElementById('to-hour')
     const moreOptionsEl = document.getElementById('more-options-el')
+    const foreHisEl = document.getElementById('fore-his')
     const todayEl = document.getElementById('today-el')
     const hourlyEl = document.getElementById('hourly-el')
     const forecastEl = document.getElementById('forecast-el')
@@ -16,16 +17,14 @@ export function renderUi () {
     burgerMenu.addEventListener('click', hideOptions)
     function hideOptions () {
         asideContent.classList.toggle('translate-left')
-        mainContent.classList.toggle('margin-left-none')
-        footerContent.classList.toggle('margin-left-none')
     }
 
     currentEl.addEventListener('click', hideCurrentContent)
     function hideCurrentContent () {
-        todayEl.classList.toggle('translate-top')
-        hourlyEl.classList.toggle('translate-top')
-        moreOptionsEl.classList.toggle('margin-top-none')
-        currentElChild.classList.toggle('height-none')
+        todayEl.classList.toggle('translate-down')
+        hourlyEl.classList.toggle('translate-down')
+        moreOptionsEl.classList.toggle('mt-8')
+        toHourEl.classList.toggle('height-28')
 
         const down = 'fi-rr-caret-down'
         const up = 'fi-rr-caret-up'
@@ -42,8 +41,9 @@ export function renderUi () {
 
     moreOptionsEl.addEventListener('click', hideMoreOptionsContent)
     function hideMoreOptionsContent () {
-        forecastEl.classList.toggle('translate-top')
-        historyEl.classList.toggle('translate-top')
+        forecastEl.classList.toggle('translate-down')
+        historyEl.classList.toggle('translate-down')
+        foreHisEl.classList.toggle('height-28')
 
         const down = 'fi-rr-caret-down'
         const up = 'fi-rr-caret-up'
@@ -57,4 +57,28 @@ export function renderUi () {
             downArrowTwo.classList.add(down)
         }
     }
+
+    function bodyBackground () {
+        async function getWeatherData (position) {
+            const body = document.querySelector('body')
+            const lat = position.coords.latitude
+            const lon = position.coords.longitude
+    
+            const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=0112e4e65c914c9591532907232608&q=${lat},${lon}&days=05`,
+            { mode: 'cors' })
+            const json = await response.json()
+            const textCondition = json.current.condition.text
+
+            const sunny = 'sunny'
+            const cloudy = 'cloudy'
+
+            if (textCondition.includes(cloudy)) {
+                body.classList.add('bg-gradient-to-r', 'from-cyan-500', 'to-blue-500')
+            }
+        }
+        const geolocation = navigator.geolocation.getCurrentPosition(getWeatherData)
+
+    }
+
+    bodyBackground()
 }
